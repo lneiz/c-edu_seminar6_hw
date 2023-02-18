f2a398d998a120a830c9c787214d5296b5f85875
lneiz <yuntsevich@ymail.com>
2023-02-19 04:54:03 +0500

    43 - done

--- Program.cs
+++ Program.cs
@@ -1,26 +1,53 @@
 ﻿//Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
 
-int[] CreateRandomArray(int m)
+// int[] CreateArray(int m)
+// {
+//     int[] array = new int[m];
+//     Console.Write("Поочередно через Enter введите все элементы массива:" + "\r\n");
+//     for(int i = 0; i < m; i++)
+//     {
+//         array[i] = Convert.ToInt32(Console.ReadLine());
+//     }
+//     return array;
+// }
+
+// void ShowResult(int[] array)
+// {
+//     int counter = 0;
+//     for(int i = 0; i < array.Length; i++)
+//     {
+//         Console.Write(array[i] + " ");
+//         if(array[i] > 0) counter++;
+//     }
+//     Console.Write("-> " + counter);
+// }
+
+// Console.WriteLine("Каков размер массива? ");
+// ShowResult(CreateArray(Convert.ToInt32(Console.ReadLine())));
+
+//Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+double[] CreateArray()
 {
-    int[] array = new int[m];
-    Console.Write("Поочередно через Enter введите все элементы массива:" + "\r\n");
-    for(int i = 0; i < m; i++)
+    double[] array = new double[4];
+    Console.Write("Поочередно через Enter введите значения b1, b2, k1 и k2:" + "\r\n");
+    for(int i = 0; i < array.Length; i++)
     {
-        array[i] = Convert.ToInt32(Console.ReadLine());
+        array[i] = Convert.ToDouble(Console.ReadLine());
     }
     return array;
 }
 
-void ShowResult(int[] array)
+double CalcX(double[] array)
 {
-    int counter = 0;
-    for(int i = 0; i < array.Length; i++)
-    {
-        Console.Write(array[i] + " ");
-        if(array[i] > 0) counter++;
-    }
-    Console.Write("-> " + counter);
+    return (array[0] - array[1]) / (-1 * array[2] + array[3]);
 }
 
-Console.WriteLine("Каков размер массива? ");
-ShowResult(CreateRandomArray(Convert.ToInt32(Console.ReadLine())));
+double CalcY(double[] array, double x)
+{
+    return (array[3] * x + array[1]);
+}
+
+double[] array = CreateArray();
+double x = CalcX(array);
+Console.Write("(" + x + ";" + CalcY(array, x) + ")");
